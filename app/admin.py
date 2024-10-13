c89a26195370b014dafb923c619dd97b90efcd36
UIT-21520962 <118161145+Khang0101@users.noreply.github.com>
2024-10-13 13:43:53 +0700

    update

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Products)
-admin.site.register(Products1)
-admin.site.register(Products2)
-admin.site.register(AdvertisingBanner)
-admin.site.register(SocialMediaLink)
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(Order)
+admin.site.register(CourseOrder)
